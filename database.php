c4cab8049ec577c5bef97ce9eb91b0d4a9864770
peterchirinos <35927974+peterchirinos@users.noreply.github.com>
2018-01-29 12:54:12 -0500

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $server = 'servicedesktintegra-mysqldbserver.mysql.database.azure.com:3306';
 $username = 'mysqldbuser@servicedesktintegra-mysqldbserver';
 $password = 'T$oport02018';
-$database = 'php_login_database';
+$database = 'mysqldatabase38707';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
